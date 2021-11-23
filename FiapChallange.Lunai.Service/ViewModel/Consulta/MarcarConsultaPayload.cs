2bf20c2d1fb535ed5db04344bfaeb1473082009b
Marcelo Martins <martinsoliveira.marcelo@outlook.com>
2021-11-23 20:25:36 -0300

    Add marcar consulta

---
+++ FiapChallange.Lunai.Service/ViewModel/Consulta/MarcarConsultaPayload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FiapChallange.Lunai.Service.ViewModel.Consulta
+{
+    public class MarcarConsultaPayload
+    {
+        [JsonPropertyName("documento")]
+        [Required]
+        public string Documento { get; set; }
+
+        [JsonPropertyName("nome_completo")]
+        [Required]
+        public string NomeCompleto { get; set; }
+
+        [JsonPropertyName("data_nascimento")]
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [JsonPropertyName("horario_consulta")]
+        [Required]
+        public string HorarioConsulta { get; set; }
+
+        [JsonPropertyName("id_hospital")]
+        [Required]
+        public Guid IdHospital { get; set; }
+    }
+}
---
+++ FiapChallange.Lunai.WebApp/Controllers/ConsultaController.cs
@@ -0,0 +1,101 @@
+﻿using FiapChallange.Lunai.Service.ViewModel.Consulta;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiapChallange.Lunai.WebApp.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        [HttpPost]
+        public IActionResult MarcarConsulta([FromBody] MarcarConsultaPayload marcarConsulta)
+        {
+            try 
+            {
+                return Ok(marcarConsulta);
+            }
+            catch(Exception ex)
+            {
+                var teste = ex;
+                return BadRequest(ex.Message);
+            }
+        }
+        //// GET: ConsultaController
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //// GET: ConsultaController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: ConsultaController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: ConsultaController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ConsultaController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ConsultaController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ConsultaController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ConsultaController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
